ee257f57bac18204659c51cd2456136f16e7d4dd
Shane Conroy <shane.f.conroy@gmail.com>
2015-04-27 11:35:42 +0100

    changed

--- Connectionist/src/mlp/MLP.java
+++ Connectionist/src/mlp/MLP.java
@@ -1,133 +1,119 @@
 package mlp;
 
 public class MLP {
+
 	private int numberInputs, numberHidden, numberOutputs;
-	private double learningRate = 0.5;
-	private double[] activations1, activations, input, hidden, output, dw1, dw2;
-	private double[][] w1, w2;
-	
-	public MLP(int numberInputs, int numberHidden, int numberOutputs) {
+	private double learningRate, input[], hidden[], output[], w1[][], w2[][];
+
+	public MLP(int numberInputs, int numberHidden, int numberOutputs, double learningRate) {
 		this.numberInputs = numberInputs;
 		this.numberHidden = numberHidden;
 		this.numberOutputs = numberOutputs;
-		
+		this.learningRate = learningRate;
+
+		// Add an extra neuron/index to each layer as a placeholder for the bias
 		input = new double[numberInputs + 1];
 		hidden = new double[numberHidden + 1];
 		output = new double[numberOutputs + 1];
-		
-		dw1 = new double[numberHidden + 1];
-		dw2 = new double[numberOutputs + 1];
-		
-		w1 = new double[numberHidden + 1][numberInputs + 1];
-		w2 = new double[numberOutputs + 1][numberHidden + 1];
-		
+
+		w1 = new double[numberHidden + 1][numberInputs + 1]; // hidden - input weights
+		w2 = new double[numberOutputs + 1][numberHidden + 1]; // output - hidden weights
+
 		randomise();
 	}
-	
-	/**
-	 * Initialise w1 and w2 to small random values. Set dw1 and dw2 to all zeroes
-	 */
-	public void randomise() {
-		for (int j = 1; j <= numberHidden; j++) {
+
+	private void randomise() {
+		// Do not include index [0][0] in loops as this will be the bias
+		for (int j = 1; j <= numberHidden; j++)
 			for (int i = 0; i <= numberInputs; i++) {
-				w1[j][i] = Math.random() - 0.5;
+				w1[j][i] = Math.random() - 0.5; // -0.5 -> 0.5
 			}
-		}
-		
-		for (int j = 1; j <= numberOutputs; j++) {
+
+		for (int j = 1; j <= numberOutputs; j++)
 			for (int i = 0; i <= numberHidden; i++) {
-				w2[j][i] = Math.random() - 0.5;
+				w2[j][i] = Math.random() - 0.5; // -0.5 -> 0.5
 			}
-		}
-		
-		initDw();
 	}
-	
-	/**
-	 * Forward pass. Input vector processed to produce an output, stored in O[]
-	 * @param inputVector
-	 */
-	public void forward(double[] inputVector) {
-		input[0] = 1.0;
-		hidden[0] = 1.0;
-		
+
+	public double[] fForward(double[] inputVector) {
+		if (inputVector.length != numberInputs) {
+			System.out.println("Input length = " + inputVector.length + "  Expected = " + numberInputs);
+			System.out.println("Input Vector Error, Incorrect Number of Inputs.");
+			System.exit(1);
+		}
+
+		// Leave room for bias at 0 index
 		for (int i = 0; i < numberInputs; i++) {
 			input[i + 1] = inputVector[i];
 		}
-		
-		//Pass through hidden layer
-		for (int j = 1; j <= numberHidden; j++) {
-			hidden[j] = 0.0;
-			for (int i = 0; i <= numberInputs; i++) {
-				hidden[j] += w1[j][i] * input[i];
-			}
-			hidden[j] = 1.0 / (1.0 + Math.exp(-hidden[j]));
-		}
-		
-		//Passing through output layer
-		for (int j = 1; j <= numberOutputs; j++) {
-			output[j] = 0.0;
-			for (int i = 0; i <= numberHidden; i++) {
-				output[j] += w2[j][i] * hidden[i];
-			}
-			output[j] = 1.0 / (1.0 + Math.exp(-output[j]));
-		}
+
+		// Set bias
+		input[0] = 1.0;
+		hidden[0] = 1.0;
+
+		// Pass through hidden layer
+		pass(numberHidden, numberInputs, hidden, w1, input);
+
+		// Pass through output layer
+		pass(numberOutputs, numberHidden, output, w2, hidden);
+
+		return output;
 	}
-	
-	/**
-	 * Compare target with output O
-	 * @param target
-	 */
-	public void backwards(double[] target) {
+
+	public double bPropagate(double[] target) {
+
 		double error = 0.0;
 		
-		for (int i = 1; i <= numberOutputs; i++) {
-			dw2[i] = output[i] * (1.0 - output[i]) * (target[i - 1] - output[i]);
+		double[] errorL2 = new double[numberOutputs + 1];
+		double[] errorL1 = new double[numberHidden + 1];
+
+		for (int i = 1; i <= numberOutputs; i++) { // Layer 2 error gradient
+			double delta = target[i - 1] - output[i];
+			error += Math.pow(delta, 2); // Always a positive error
+			// If delta == 0, disregard. Else the error is the delta * output[i] * (1.0 - output[i])
+			errorL2[i] = output[i] * (1.0 - output[i]) * delta;
 		}
-		
-		for (int i = 0; i <= numberHidden; i++) {
-			for (int j = 1; j < numberOutputs; j++) {
-				error += w2[j][i] * dw2[j];
-			}
-			
-			dw1[i] = hidden[i] * (1.0 - hidden[i]) * error;
-			System.out.println("error : " + error);
-			error = 0.0;
+
+		double delta = 0.0; // Summation of all hidden -> output weights (per hidden neuron) plus their calculated errors.
+		for (int i = 0; i <= numberHidden; i++) {  // Layer 1 error gradient
+			for (int j = 1; j <= numberOutputs; j++)
+				delta += w2[j][i] * errorL2[j];
+
+			errorL1[i] = hidden[i] * (1.0 - hidden[i]) * delta;
+			delta = 0.0;
 		}
-		
-		updateWeights(learningRate);
+
+		// Update the weights (learningRate * error in this neuron * selected input to this neuron.)
+		for (int j = 1; j <= numberHidden; j++)
+			for (int i = 0; i <= numberInputs; i++)
+				w1[j][i] += learningRate * errorL1[j] * input[i];
+
+		for (int j = 1; j <= numberOutputs; j++)
+			for (int i = 0; i <= numberHidden; i++)
+				w2[j][i] += learningRate * errorL2[j] * hidden[i];
+
+		return error / (numberOutputs + 1); // The more outputs, the more the error increments.
 	}
-	
-	/**
-	 * this simply does (component by component, i.e. within for loops):
-	 * W1 += learningRate*dW1;
-	 * W2 += learningRate*dW2;
-	 * reset dw1,dw2
-	 * @param learningRate
-	 */
-	public void updateWeights(double learningRate) {
-		for (int j = 1; j <= numberOutputs; j++) {
-			for (int i = 0; i <= numberHidden; i++) {
-				w2[j][i] += learningRate * dw2[j] * hidden[i];
-			}
-		}
-		
-		for (int j = 1; j <= numberHidden; j++) {
-			for (int i = 0; i <= numberInputs; i++) {
-				w1[j][i] += learningRate * dw1[j] * input[i];
-			}
-		}
-		
-		initDw();
+
+	public void setLearningRate(double learningRate) {
+		this.learningRate = learningRate;
 	}
-	
-	private void initDw() {
-		for (int i = 0; i <= numberHidden; i++) {
-			dw1[i] = 0;
-		}
-		
-		for (int i = 0; i <= numberOutputs; i++) {
-			dw2[i] = 0;
+
+	private double sigmoid(double x) {
+		// Sigmoid using logistic function
+		return 1.0 / (1.0 + Math.exp(-x));
+	}
+
+	// Pass from one layer to next in ff method
+	private void pass(int jLimit, int iLimit, double[] outputLayer, double[][] weights, double[] inputs) {
+		for (int j = 1; j <= jLimit; j++) {
+			outputLayer[j] = 0.0;
+			// For all the inputs to neuron add them * their weights
+			for (int i = 0; i <= iLimit; i++) {
+				outputLayer[j] += inputs[i] * weights[j][i];
+			}
+			// Activation function
+			outputLayer[j] = sigmoid(outputLayer[j]);
 		}
 	}
 }
--- Connectionist/src/mlp/Main.java
+++ Connectionist/src/mlp/Main.java
@@ -1,16 +1,76 @@
 package mlp;
 
+import java.util.Arrays;
+
 public class Main {
-	private static final int maxEpochs = 10;
-	
+	private static final int MAX_EPOCHS = 500000;
+	private static final double ERR_THRESHOLD = 0.01;
+
 	public static void main(String[] args) {
-		double[][] example = {{0, 0, 0}, {0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
-		MLP NN = new MLP(2, 2, 1);
-		for (int e = 0; e < maxEpochs; e++) {
-			for (int p = 0; p < 4; p++) {
-				NN.forward(new double[]{example[p][0], example[p][1]});
-				NN.backwards(new double[]{example[p][2]});
+		double error = 0.0;
+
+		// QUESTION 1
+		MLP q1 = new MLP(2, 2, 1, 0.5);
+		double[][] trainingSet = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+		double[][] target = { { 0 }, { 1 }, { 1 }, { 0 } };
+
+		for (int i = 0; i < MAX_EPOCHS; i++) {
+			error = 0.0;
+			for (int j = 0; j < trainingSet.length; j++) {
+				q1.fForward(trainingSet[j]);
+				error += q1.bPropagate(target[j]);
+			}
+			if (i % 200 == 0)
+				System.out.println("Epoch: " + i + "  Error: " + error);
+			if (error < ERR_THRESHOLD) {
+				System.out.println("END, Error < " + ERR_THRESHOLD + ": " + error);
+				break;
 			}
 		}
+
+		// QUESTION 2 Check if examples are predicted correctly
+		error = 0.0;
+		for (int i = 0; i < trainingSet.length; i++) {
+			if (Math.round(q1.fForward(trainingSet[i])[1]) == target[i][0])
+				System.out.println("CORRECT");
+			else
+				System.out.println("INCORRECT");
+			error = q1.bPropagate(target[i]);
+		}
+		System.out.println("Test set error: " + error);
+
+		// QUESTION 3
+		double[][] trainingSet2 = new double[50][4];
+		double[][] target2 = new double[50][1];
+		for (int j = 0; j < 50; j++) {
+			double sum = 0.0;
+			for (int i = 0; i < 4; i++) {
+				trainingSet2[j][i] = (Math.random() * 2.0) - 1.0;
+				sum += trainingSet2[j][i];
+			}
+			target2[j][0] = Math.sin(sum);
+		}
+		MLP q3 = new MLP(4, 5, 1, 0.01);
+		for (int i = 0; i < MAX_EPOCHS; i++) {
+			error = 0.0;
+			for (int j = 0; j < 40; j++) {
+				q3.fForward(trainingSet2[j]);
+				error += q3.bPropagate(target2[j]);
+			}
+			if (i % 200 == 0)
+				System.out.println("Epoch: " + i + "  Error: " + error);
+			if (error < ERR_THRESHOLD) {
+				System.out.println("END, Error < " + ERR_THRESHOLD + ": " + error);
+				break;
+			}
+		}
+		
+		// QUESTION 4 Testing
+		error = 0.0;
+		for (int j = 40; j < 50; j++) {
+			q3.fForward(trainingSet2[j]);
+			error = q3.bPropagate(target2[j]);
+		}
+		System.out.println("Test set error: " + error);
 	}
 }
